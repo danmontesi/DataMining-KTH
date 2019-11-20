ce6d64443db07cb4b9f7f34c1296d0e5ef4ed329
dev-strenuus <andrea5.scotti@mail.polimi.it>
2019-11-20 23:51:09 +0100

    comment on cardinality

--- lab3/centralityCalculator.py
+++ lab3/centralityCalculator.py
@@ -5,7 +5,7 @@ class CentralityCalculator:
 
     def __init__(self, graph):
         self.graph = graph
-        self.hyper_ball_cardinalities = np.zeros((self.graph.num_of_nodes, self.graph.num_of_nodes + 1))
+        self.hyper_ball_cardinalities = np.zeros((self.graph.num_of_nodes, self.graph.num_of_nodes + 1)) #a matrix that store, for each node x and for each radius r, the cardinality of the HyperBall B(x,r)
 
     def calculate_closeness(self, node):
         return
